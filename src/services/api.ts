857c6a1695e4cec99d0c6501f369cd5c546b051c
Julio Muller <juliodolago@gmail.com>
2021-08-06 00:38:14 -0300

    Add an interceptor to embed token on requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,42 @@
-import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios';
 
+/**
+ * Create a brand new instance of 'axios' to handle requests to 'mirage'
+ */
 const http: AxiosInstance = axios.create({
   baseURL: 'https://fuerza.test',
 });
 
+/**
+ * Set default headers
+ */
 http.defaults.headers.post['Content-Type'] = 'application/json';
 
+/**
+ * Retrieve and add access token to all requests calls
+ *
+ * ===========================================================================
+ * (assuming the server requires the token in a header called "authorization")
+ * ===========================================================================
+ */
+http.interceptors.request.use((config: AxiosRequestConfig) => {
+  try {
+    const authStorage = localStorage.getItem('FORZA::auth');
+    const token = authStorage && JSON.parse(authStorage)?.token;
+
+    if (token) {
+      config.headers.authorization = `Bearer ${token}`;
+    }
+  } catch (error) {
+    console.log('Unable to retrieve token from storage', 'error');
+  }
+
+  return config;
+});
+
+/**
+ * Expose response data to service client
+ */
 http.interceptors.response.use(
   async (response: AxiosResponse): Promise<any> => {
     if (response.status >= 200 && response.status < 300) {
