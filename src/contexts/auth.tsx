e384a02f0cc270b47c6c19395656252443d42bde
Julio Muller <juliodolago@gmail.com>
2021-08-06 00:39:40 -0300

    Implement AuthContext state/handlers

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,29 +1,59 @@
-import { createContext, FC, useState } from 'react';
+import { createContext, FC } from 'react';
+import { useLocalStorage } from 'react-use';
+import http from '../services/api';
+
+type User = {
+  id: number;
+  email: string;
+  username: string;
+};
+
+type Storage = {
+  user: User;
+  token: string;
+};
 
 type AuthContextProps = {
+  user?: User;
   isAuthenticated: boolean;
-  signIn: () => void;
+  signIn: (username: string, password: string) => void;
+  signUp: (username: string, password: string, email?: string) => void;
   signOut: () => void;
 };
 
 export const AuthContext = createContext({} as AuthContextProps);
 
 export const AuthProvider: FC = ({ children }) => {
-  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [storage, setStorage, destroyStorage] = useLocalStorage<Storage>('FORZA::auth');
 
-  function signIn() {
-    setAuthenticated(true);
+  const user = storage?.user;
+  const isAuthenticated = Boolean(user);
+
+  async function signIn(username: string, password: string) {
+    const data = { username, password };
+    const storage: Storage = await http.post('/auth/login', data);
+
+    setStorage(storage);
   }
 
-  function signOut() {
-    setAuthenticated(false);
+  async function signUp(username: string, password: string, email?: string) {
+    const data = { email, username, password };
+    const storage: Storage = await http.post('/auth/signup', data);
+
+    setStorage(storage);
+  }
+
+  async function signOut() {
+    destroyStorage();
   }
 
   return (
     <AuthContext.Provider
       value={{
+        user,
         isAuthenticated,
         signIn,
+        signUp,
         signOut,
       }}
     >
