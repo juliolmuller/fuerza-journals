c3649015e283d0ea532f7aedb581f214493d6855
juliolmuller <juliolmuller@outlook.com>
2025-01-24 17:38:57 -0300

    refactor(deps): update `react` and `react-dom` to latest version

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from '~/App';
 import { setupServer } from '~/services/mirage/server';
@@ -8,9 +8,8 @@ if (import.meta.env.DEV) {
   setupServer();
 }
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root'),
 );
